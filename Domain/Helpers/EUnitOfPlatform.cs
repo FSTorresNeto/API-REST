f89e2f526c976797f1a070befac5e5dbca512d79
Torres Neto <70548394+FSTorresNeto@users.noreply.github.com>
2022-01-14 05:08:15 -0300

    Add files via upload

---
+++ Domain/Helpers/EUnitOfPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace api_rest.Domain.Helpers
+{
+    public enum EUnitOfPlatform : byte
+    {
+        [Description("Xbox")]
+        Xbox = 1,
+
+        [Description("PlayStation")]
+        PlayStation = 2,
+
+        [Description("Nitendo")]
+        Nitendo = 3,
+
+        [Description("PC")]
+        PC = 4,
+
+        [Description("AllPlatforms")]
+        AllPlatforms = 5
+    }
+
+}
